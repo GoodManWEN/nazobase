9b0406da748f38a4b8bdda41d142de53918c435a
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2019-05-04 04:06:51 +0800

    update and bugs fixes

--- nazobase.py
+++ nazobase.py
@@ -5,17 +5,27 @@ except:
 
 import vapoursynth as vs
 from os import path
-from inspect import signature,getframeinfo,currentframe
-from functools import wraps
+from inspect import signature, getframeinfo, currentframe
+from functools import wraps, partial
 from re import search
-import nazobase as nazo
 
 ######################################################
 #
 #           nazobase distribution
-#           version 0.1.15
+#           version 0.1.17
 #
 ######################################################
+
+'''
+functinos:
+    dataloader
+    check
+    diff
+    quickresize
+    print
+    getplane
+    autocheck
+'''
 
 def autocheck(func):
     '''
@@ -32,6 +42,7 @@ def autocheck(func):
             values , types_ = (), target[name]
         else:
             values = tuple(filter(lambda x:False if isinstance(x,type) else True ,target[name] ))
+            # due to filtering by set ,list* could not be used in type hint
             types_ = tuple( set(target[name]) - set(values) )
         if value in values or isinstance(value, types_):
             return ;
@@ -81,13 +92,15 @@ def dataloader(source:str, first:(None,int) = None, last:(None,int) = None) -> v
     Attributes:
 
         string source:
-            your source path
+            Your source path
+            Last two character of each node matters,
+            cause depth conversion depends on them.
 
         (int first:)
                     \
         
         (int last:)
-            returns the frames between the arguments first and last
+            Returns the frames between the arguments first and last
 
 
     Usage:
@@ -97,7 +110,7 @@ def dataloader(source:str, first:(None,int) = None, last:(None,int) = None) -> v
 
         core = vs.core
 
-        src8,src10,src16 = nazo.dataloader('video.mp4')
+        src8, src10, src16 = nazo.dataloader('video.mp4')
         src16.set_output()
 
         ↑ Thus you'll get three clips with specific depth respectively
@@ -153,7 +166,7 @@ def dataloader(source:str, first:(None,int) = None, last:(None,int) = None) -> v
 
 # @autocheck
 # decorator affacts inspect debuging process should not be used
-def check(clipa:vs.VideoNode, *clipbs:vs.VideoNode) -> vs.VideoNode:
+def check(clipa:vs.VideoNode, *clipbs:vs.VideoNode, **kwargs) -> vs.VideoNode:
 
     '''
     Automatically adjust format/bit depth/FPS of clip those in clips to clipa,
@@ -169,7 +182,7 @@ def check(clipa:vs.VideoNode, *clipbs:vs.VideoNode) -> vs.VideoNode:
         VideoNode clipa:
             target videonode for adjusting.
 
-        *(VideoNode clipbs):
+        *(VideoNode clipbs:)
             one or more videonode to adjust.
             of course you can also leave it empty.
     
@@ -187,6 +200,14 @@ def check(clipa:vs.VideoNode, *clipbs:vs.VideoNode) -> vs.VideoNode:
 
         nazo.check(nazo.getplane(src10,1) , core.tcanny.Tcanny(src10) ).set_output()
 
+    Wrong use:
+
+        from nazobase import *
+        
+        ...
+
+        check(check(clipa, clipb), clipc).set_output()
+
     '''
 
     # initialization
@@ -200,6 +221,9 @@ def check(clipa:vs.VideoNode, *clipbs:vs.VideoNode) -> vs.VideoNode:
     clipbs = list(clipbs)
     target_color_family = clipa.format.color_family
     target_bit_depth = clipa.format.bits_per_sample
+
+    # check if function is called as api
+    mode_1_flag = True if ('mode' in kwargs and kwargs['mode'] != 0) else False
 
     # backtracking node list
     def _pop_stack():
@@ -216,18 +240,18 @@ def check(clipa:vs.VideoNode, *clipbs:vs.VideoNode) -> vs.VideoNode:
                     ',':lambda :node_stack.append('') ,
                     ' ':lambda :None,
                     ')':_pop_stack      }
-
-    try:
-        node_stack = list()
-        activate_string = getframeinfo(currentframe().f_back)[3][0]
-        activate_string = search('check[\s]*\(.+\)(\.set_output\(\))*',activate_string).group().replace(".set_output()","")
-        activate_string = activate_string[activate_string.index('('):len(activate_string)-activate_string[::-1].index(')')]   
-    except:
-        node_stack = [str(clipa)]
-    else:
-        for char in activate_string:
-            try: char_opts[char]()
-            except: node_stack[-1] += char
+    if not mode_1_flag:
+        try:
+            node_stack = list()
+            activate_string = getframeinfo(currentframe().f_back)[3][0]
+            activate_string = search('check[\s]*\(.+\)(\.set_output\(\))*',activate_string).group().replace(".set_output()","")
+            activate_string = activate_string[activate_string.index('('):len(activate_string)-activate_string[::-1].index(')')]   
+        except:
+            node_stack = [str(clipa)]
+        else:
+            for char in activate_string:
+                try: char_opts[char]()
+                except: node_stack[-1] += char
    
     # options table
     def operate_YUV(clip):
@@ -253,7 +277,7 @@ def check(clipa:vs.VideoNode, *clipbs:vs.VideoNode) -> vs.VideoNode:
 
         # modifying bit depth
         if clipbs[num].format.bits_per_sample != target_bit_depth:
-            clipbs[num] = Depth(clipbs[num],clipbs[num].format.bits_per_sample)
+            clipbs[num] = Depth(clipbs[num],target_bit_depth)
 
         # modifying resolusion
         if clip.width != clipa.width or clip.height != clipa.height:
@@ -261,8 +285,12 @@ def check(clipa:vs.VideoNode, *clipbs:vs.VideoNode) -> vs.VideoNode:
 
         # post processing
         clipbs[num] = core.std.AssumeFPS(clipbs[num], clipa)
-        clipbs[num] = core.text.Text(clipbs[num], node_stack[num+1])
+        if not mode_1_flag:
+            clipbs[num] = core.text.Text(clipbs[num], node_stack[num+1])
     
+    if mode_1_flag:
+        return [clipa] + clipbs
+
     clipa = clipa.text.Text(node_stack[0])
     output_nodes = [clipa] + clipbs
     
@@ -271,7 +299,148 @@ def check(clipa:vs.VideoNode, *clipbs:vs.VideoNode) -> vs.VideoNode:
 
 
 @autocheck
-def print(clip:vs.VideoNode , string:str = '') -> vs.VideoNode:
+def diff(clipa:vs.VideoNode , clipb:vs.VideoNode , amp:int = 10, planes:(None,0,1,2,list) = None, binarize:bool = False, maskedmerge:bool = False) -> vs.VideoNode:
+
+    '''
+
+    Easy function helps you check out differences between clips.
+    Expression : luminance = abs( clipa - clipb ) * amp
+
+    ###############################################
+    Attributes:
+
+        VideoNode clipa:
+            \
+
+        VideoNode clipb:
+            Format insensitive ,
+            Clipb will be converted into the vary format of clipa at first.
+
+        (int amp:) 
+            Adjust strength of the amplifier.
+
+        (int[] planes:)
+            \
+
+        (bool binarize:)
+            If true, each pair of pixel who are not exactly the same
+            will be highlight labeled. 
+
+        (bool maskedmerge:)
+            Showing a merged picture of different pixels like using alpha channel.
+            Trun on this if you don't like check(src1,diff(src1,src2)).
+
+    '''
+
+    core = vs.core
+    funcName = 'diff'
+
+    # normalization & init
+    clipa, clipb = partial(check ,mode = 1)(clipa, clipb)
+
+    if planes == None:
+        planes = list(range(clipa.format.num_planes))
+    elif isinstance(planes,int):
+        planes = [planes]
+    if len(planes) == 1:
+        clipa = clipa.std.ShufflePlanes(planes,vs.GRAY)
+        clipb = clipb.std.ShufflePlanes(planes,vs.GRAY)
+
+    # expr & calculate difference
+    expr = f"x y - abs {amp} *"
+    res = core.std.Expr([clipa, clipb], [expr])
+    res_b = res.std.Binarize(threshold = 1, planes = planes if len(planes) > 1 else [0])
+    # create maskmerged clip if required
+    if maskedmerge:
+        blankclip = core.std.BlankClip(width = clipa.width, height = clipa.height,  format = vs.YUV420P8, length = clipa.num_frames, fpsnum = clipa.fps_num, fpsden = clipa.fps_den ,color = [250,60,115])
+        blankclip , clipa = partial(check ,mode = 1)(blankclip, clipa)
+        return core.std.MaskedMerge(clipa, blankclip, Depth(res_b.std.Expr('x 2 /'),8),  [0,1,2], True)
+    elif binarize:
+        return res_b
+    
+    return res
+
+
+
+@autocheck
+def quickresize(clip:vs.VideoNode, width:(None,int) = None, height:(None,int) = None, kernel:str = "Spline16" ,**resize_kwargs) -> vs.VideoNode:
+    
+    '''
+    resize avoids resampling lose in YUV/RGB converting ,
+    use this if there's no need to change color space.
+
+    Default using BT709 & TVrange for animation processing.
+
+    ###############################################
+    Attributes:
+
+        VideoNode clip:
+            Target.
+
+        (int width:)
+            Width will not change if not specified.
+
+        (int height:)
+            Height will not change if not specified.
+
+        (str Kernel:)
+            The resize kernel we used is components from vapoursynth standard library. Input as string and it will be converted into function by black magic eval().
+
+            Case insensitive.
+
+        (resize_kwargs:)
+            Custom resize attributes if you want.
+
+    Usage:
+
+        from nazobase import *
+
+        src = dataloader('video.mp4')
+        # convert with bicubic b/c = 1/3
+        res = quickresize(src, 1920, 1080, 'bicubic', filter_param_a = 1/3, filter_param_b = 1/3)
+
+        res.set_output()
+    
+    '''
+
+    # Using BT709 & fulls/fulld=False by default for animation processing.
+    core = vs.core
+    funcName = "quickresize"
+    kernel_id = "com.vapoursynth.resize"
+    matrix = "709"
+
+    # obtain pointer of resizer
+    plugins = core.get_plugins()
+    functions = plugins[kernel_id]["functions"]
+    functions = dict(zip(map(lambda x:x.upper(),functions) ,functions))
+    try:
+        operator = eval(f"core.{plugins[kernel_id]['namespace']}.{functions[kernel.upper()]}")
+    except:
+        raise KeyError(f"Didn't find kernel {kernel} in vscore with kernel_id {kernel_id} ,check it again." )
+    
+    # 
+    clip = Depth(clip,16) if clip.format.bits_per_sample < 16 else clip
+
+    # processing
+    if clip.format.color_family is vs.YUV:
+        clipY = core.std.ShufflePlanes(clip,0,vs.GRAY).fmtc.transfer(transs=matrix,transd="linear",fulls=False,fulld=False)
+        clipY = operator(clipY,width = width ,height = height , **resize_kwargs).fmtc.transfer(transs="linear",transd=matrix,fulls=False,fulld=False)
+        clipC = operator(clip, width=width,height = height,**resize_kwargs)
+        res = core.std.ShufflePlanes([clipY,clipC], [0,1,2], vs.YUV)
+    elif clip.format.color_family is vs.GRAY:
+        res = operator(clip.fmtc.transfer(transs=matrix,transd="linear",fulls=False,fulld=False), width = width, height = height, ** resize_kwargs).fmtc.transfer(transs="linear",transd=matrix,fulls=False,fulld=False)
+    else:
+        raise Warning('This function is particularly designed for clips in YUV|GRAY color family.')
+
+    if orn_bitdepth < res.format.bits_per_sample :
+        res = Depth(res, orn_bitdepth)
+
+    return res
+
+
+
+@autocheck
+def print(clip:vs.VideoNode , string = '') -> vs.VideoNode:
 
     '''
     used to show variables directly.
@@ -299,10 +468,3 @@ def getplane(clip:vs.VideoNode , plane:int = 0) -> vs.VideoNode:
     funcName = 'getplane'
 
     return core.std.ShufflePlanes(clip,plane,vs.GRAY)
-
-
-
-@autocheck
-def diff(clipa:vs.VideoNode , clipb:vs.VideoNode , amp:int = 8, planes:(None,int,list) = None) -> vs.VideoNode:
-    # under construction
-    return clipa
--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 #############################################
 # File Name: setup.py
 # Author: WEN
-# Created Time:  2019-05-01 00:00:00
+# Created Time:  2019-05-04 03:54:00
 #############################################
 
 from setuptools import setup
 
 setup(
     name            ='nazobase',
-    version         ='0.1.13',
+    version         ='0.1.17',
     py_modules      =['nazobase'],
     author          = 'WEN',
     license         = "LGPLv3",
     description     = "NAZOrip's basement",
+    url             = "www.nazorip.site",
     # install_requires=[
     #     '',
     # ],
@@ -22,7 +23,7 @@ setup(
     ''',
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
 )
