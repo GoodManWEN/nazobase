1e7f06a714ae2e9f37df3ed90331cc3aa45f9389
GoodManWEN <44322449+GoodManWEN@users.noreply.github.com>
2020-02-08 12:40:06 +0800

    fixed bugs

--- PostModify.py
+++ PostModify.py
@@ -21,14 +21,36 @@ def fix(file ,replacement):
     with open(file,'w',encoding='utf-8') as f:
         f.write(out)
 
+# Since CI/CD environment can not support vs install 
+# we need to findout other means where we don't need to import package.
+
+with open('testbase.py','r',encoding='utf-8') as f:
+    cont = f.readlines() 
+cont.insert(0,'panaceafunc = lambda *args,**kwargs:None\n')
+
+for i , line in enumerate(cont):
+    if line[:4] != ' '*4 and (re.search('from(.*?)import(.*?)',line) or re.search('import(.*?)as(.*?)',line)):
+        if '.' in line:
+            if ' as ' in line:
+                libs = line[line.index(' as ')+4:].split(',')
+            else:
+                libs = line[line.index('import ')+7:].split(',')
+            libs = list(map(lambda x:x.strip(), libs))
+            cont.pop(i)
+            cont.insert(i,f"{','.join(libs)} = {','.join(['panaceafunc'] * len(libs))}\n")
+        if 'vapoursynth' in line:
+            cont.insert(i+1,'vs.VideoNode=panaceafunc\n')
+            cont.insert(i+1,'vs=new()\n')
+            cont.insert(i+1,'class new:pass\n')
+            
+with open('testbase.py','w',encoding='utf-8') as f:
+    f.writelines(cont)
+
 import testbase
 from testbase import *
 
 with open('testbase.py','r',encoding='utf-8') as f:
     cont = f.read()
-# Since CI/CD environment can not support vs install 
-# we need to findout other means where we don't need to import package.
-
 var_cont = dir(testbase)
 func_name_list = list()
 for var in var_cont:
